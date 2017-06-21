33624ef31c05c870d197316117c6ca9721d8e494
Raquel Ambrozio <raquel.fonseca@ccc.ufcg.edu.br>
2017-06-21 14:06:41 -0300

    Aparecendo o Post da série, porém nada fazendo ainda com ele

--- js/controllers/minhasSeriesCtrl.js
+++ js/controllers/minhasSeriesCtrl.js
@@ -1,30 +1,30 @@
 app.controller("minhasSeriesCtrl", function ($scope, $http) {
 	
 	$scope.app = "MINHAS SÉRIES";
-	$scope.seriesMeuPerfil = [];  <!-- array de series-->
+	$scope.seriesMeuPerfil = [];
+	$scope.seriesPesquisadas = [];
 
 	$scope.procuraSerie = function(nome) {
-
-		$http.get("http://www.omdbapi.com/?t=" + nome+ "&type=series&apikey=93330d3c").then(successCallback, errorCallback);
+		$http.get("http://www.omdbapi.com/?s=" + nome+ "&type=series&apikey=93330d3c").then(successCallback, errorCallback);
 
 			function successCallback(response){
 				console.log(response);
 
-			if (response.data.Response === 'False') {
-        		alert("Série não encontrada!");
+
+
+				if (response.data.Response === 'False') {
+    	    		alert("Série não encontrada!");
+				}
+				else {
+					$scope.seriesPesquisadas = response.data.Search;
+
+
+				}
 			}
 
-			}
 			function errorCallback(error){
     			console.log("erro");
 			}
-
-	}
-
-
-
-	$scope = function adionaSerie(nome) {
-		$scope.seriesMeuPerfil.push(nome);
 	}
 
 
